cff726b011ce36aea263720c59da4ffaa84f0c32
ar00gula <arianna.mayer@gmail.com>
2021-01-17 13:19:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2
 // Write your code here
+
+let mod = 16 % 6
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let max = Math.max(1, 2, 20)
